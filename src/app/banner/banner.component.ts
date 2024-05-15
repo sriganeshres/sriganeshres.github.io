330f4fe83ddb099e794bffa00662055037149a07
sriganeshres <sriganeshthota12345@gmail.com>
2024-05-16 01:01:16 +0530

    UI Changes

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatIconModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
@@ -13,10 +14,25 @@ export class BannerComponent {
   onClick() {
     this.showSideMenu = !this.showSideMenu;
   }
+  getText() {
+  if (!this.showSideMenu) return '\u2630'; // Unicode character for ☰ (Hamburger menu)
+  else return '\u2715'; // Unicode character for ✕ (Close icon)
+  }
+
   getColors() {
-    const common = "pt-12 flex-col justify-start gap-4 h-full fixed top-12 left-0 bg-gray-500 w-1/2 text-center ease-in-out duration-1000";
+    const common = "pt-12 flex-col justify-start gap-4 w-full z-20 fixed top-[80px] h-full left-0 bg-[#1571AF] text-center ease-in-out duration-1000";
     const conditional = this.showSideMenu ? "flex" : "hidden"
     return `${common} ${conditional}`;
+  }
+
+  getColors2() {
+  const common = "w-1/2 flex-row justify-center gap-4 w-full z-20 fixed bottom-10 left-0 bg-[#1571AF] text-center ease-in-out duration-1000";
+  const conditional = this.showSideMenu ? "flex" : "hidden"
+    return ` ${conditional} ${common}`;
+  }
+
+  onClickPage() {
+    this.showSideMenu = false;
   }
 
 }

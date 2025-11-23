b49867dd2bc3d7028608814ebfe59cdbebb90b2e
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2025-11-23 05:41:47 +0000

    update portfolio

--- src/app/_models/tags.ts
+++ src/app/_models/tags.ts
@@ -8,38 +8,32 @@ export class Tag {
     static readonly SPRINGWEB = new Tag('Spring Web', '#5a7581');
     static readonly LOMBOK = new Tag('Lombok', '#5a7581');
     static readonly JPA = new Tag('JPA', '#5a7581');
-    static readonly JDBC = new Tag('JDBC', '#5a7581');
     static readonly POSTGRESQL = new Tag('PostgreSQL', '#355158')
     static readonly SQL = new Tag('SQL', '#355158');
-    static readonly SQLALCHEMY = new Tag('SQLAlchemy', '#4a6a77');
     static readonly PYTHON = new Tag('Python', '#4a6a77');
-    static readonly FLASK = new Tag('Flask', '#4a6a77');
-    static readonly BEAUTIFULSOUP = new Tag('Beautiful Soup', '#4a6a77');
-    static readonly PLAYWWRIGHT = new Tag('Playwright', '#4a6a77');
-    static readonly SELENIUM = new Tag('Selenium Driver', '#4a6a77');
-    static readonly PANDAS = new Tag('Pandas', '#4a6a77');
-    static readonly DASH = new Tag('Dash', '#4a6a77');
-    static readonly PLOTLY = new Tag('Plotly', '#4a6a77');
     static readonly RESTAPI = new Tag('Rest API', '#355158');
-    static readonly INSOMNIA = new Tag('Insomnia', '#355158');
-    static readonly POSTMAN = new Tag('Postman', '#355158');
     static readonly TYPESCRIPT = new Tag('Typescript', '#192129');
     static readonly JAVASCRIPT = new Tag('Javascript', '#192129');
     static readonly HTML5 = new Tag('HTML5', '#192129');
     static readonly CSS3 = new Tag('CSS3', '#192129');
-    static readonly BOOTSTRAP = new Tag('Bootstrap', '#192129');
-    static readonly PYTEST = new Tag('Pytest', '#4a6a77');
-    static readonly JUNIT = new Tag('JUnit', '#5a7581');
-    static readonly MOCKITO = new Tag('Mockito', '#5a7581');
+    static readonly TAILWINDCSS = new Tag('Tailwind CSS', '#192129');
     static readonly DOCKER = new Tag('Docker', '#355158');
     static readonly DOCKERCOMPOSE = new Tag('Docker Compose', '#355158');
-    static readonly VUEJS = new Tag('Vue.JS', '#192129');
     static readonly HIBERNATE = new Tag('Hibernate', '#5a7581');
     static readonly FASTAPI = new Tag('FastAPI', '#4a6a77');
     static readonly SQLITE = new Tag('SQLite', '#355158');
-    static readonly PYDANTIC = new Tag('Pydantic', '##4a6a77');
     static readonly GOLANG = new Tag('Golang', '#457189');
-    static readonly RABBITMQ = new Tag('RabbitMQ', '#457189' )
+    static readonly CPP = new Tag('C++', '#00599C');
+    static readonly REDIS = new Tag('Redis', '#A41E11');
+    static readonly KAFKA = new Tag('Kafka', '#231f20');
+    static readonly OAuth = new Tag('OAuth', '#192129');
+    static readonly MATERIALUI = new Tag('Material UI', '#192129');
+    static readonly REACT = new Tag('React', '#4FAA41');
+    static readonly KOTLIN = new Tag('Kotlin', '#5a7581');
+    static readonly RUST = new Tag('Rust', '#DEA584');
+    static readonly BOOTSTRAP = new Tag('Bootstrap', '#563d7c');
+    static readonly FIREBASE = new Tag('Firebase', '#f58220');
+
 
 
 
